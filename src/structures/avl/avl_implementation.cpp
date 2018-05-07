1548c8cd214c0b8a2b58ec3c6862378d87b175ee
Sima214 <anastasios2014@gmail.com>
2018-05-08 00:06:44 +0300

    Avl search

--- src/structures/avl/avl_implementation.cpp
+++ src/structures/avl/avl_implementation.cpp
@@ -1,1 +1,27 @@
 #include "avl_implementation.hpp"
+
+template<class T> bool Avl<T>::find(T key) {
+  __AVLNode<T>* p;
+  __AVLNode<T>* found = this->__find(key, &p);
+  return found != NULL;
+}
+
+template<class T> __AVLNode<T>* Avl<T>::__find(T k, __AVLNode<T>** p) {
+  __AVLNode<T>* c = this->root;
+  while(c != NULL) {
+    if(c->value == k) {
+      return c;
+    }
+    else if(c->value < k) {
+      *p = c;
+      c = c->left;
+    }
+    else if(c->value > k) {
+      *p = c;
+      c = c->right;
+    }
+  }
+  return c;
+}
+
+template class Avl<int>;
--- src/structures/avl/avl_implementation.hpp
+++ src/structures/avl/avl_implementation.hpp
@@ -3,10 +3,29 @@
 
 #include <dispatch/icommandable.hpp>
 
+template <class T> struct __AVLNode {
+  T value;
+  char meta;
+  __AVLNode<T>* left;
+  __AVLNode<T>* right;
+};
+
 /*
  * Generic AVL class
  */
 template<class T> class Avl {
-  //TODO: implement with templates.
+  public:
+    /*
+     * No-op constructor.
+     */
+    Avl(){
+      root = NULL;
+    };
+    bool find(T);
+    bool insert(T);
+    bool del(T);
+  private:
+    __AVLNode<T>* root;
+    __AVLNode<T>* __find(T key, __AVLNode<T>** parent);
 };
 #endif /*AVLIMP_STRUCT_HPP*/
